206ee54348873c5e112c965dff48d38c49096713
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-02-10 19:47:41 -0500

    added interests

--- js/app.js
+++ js/app.js
@@ -10,3 +10,30 @@ function flipBack(el){
 	el.children[1].style.transform = "perspective(600px) rotateY(0deg)";
 	el.children[0].style.transform = "perspective(600px) rotateY(180deg)";
 }
+
+function interestDetail(){
+	$('.buddy').hide();
+	$('.icons > li > i').on('mouseenter', function(){
+		$(this).siblings().fadeIn();
+	})
+	$('.icons > li > i').on('mouseleave', function(){
+		$(this).siblings().fadeOut();
+	})
+}
+
+$(document).ready(function(){
+	interestDetail();
+function flip(el){
+	el.children[1].style.webkitTransform = "perspective(600px) rotateY(-180deg)";
+	el.children[0].style.webkitTransform = "perspective(600px) rotateY(0deg)";
+	el.children[1].style.transform = "perspective(600px) rotateY(-180deg)";
+	el.children[0].style.transform = "perspective(600px) rotateY(0deg)";
+}
+function flipBack(el){
+	el.children[1].style.webkitTransform = "perspective(600px) rotateY(0deg)";
+	el.children[0].style.webkitTransform = "perspective(600px) rotateY(180deg)";
+	el.children[1].style.transform = "perspective(600px) rotateY(0deg)";
+	el.children[0].style.transform = "perspective(600px) rotateY(180deg)";
+}
+
+});
