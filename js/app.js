1bf9df9bf4f2373132dbec516d94761cac66921d
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-02-12 15:50:02 -0500

    button styles

--- js/app.js
+++ js/app.js
@@ -23,18 +23,6 @@ function interestDetail(){
 
 $(document).ready(function(){
 	interestDetail();
-	function flip(el){
-		el.children[1].style.webkitTransform = "perspective(600px) rotateY(-180deg)";
-		el.children[0].style.webkitTransform = "perspective(600px) rotateY(0deg)";
-		el.children[1].style.transform = "perspective(600px) rotateY(-180deg)";
-		el.children[0].style.transform = "perspective(600px) rotateY(0deg)";
-	}
-	function flipBack(el){
-		el.children[1].style.webkitTransform = "perspective(600px) rotateY(0deg)";
-		el.children[0].style.webkitTransform = "perspective(600px) rotateY(180deg)";
-		el.children[1].style.transform = "perspective(600px) rotateY(0deg)";
-		el.children[0].style.transform = "perspective(600px) rotateY(180deg)";
-	}
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
 	    margin:10,
