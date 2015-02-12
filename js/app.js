6f0c80ff479f8952e047b2eb2ca59df1573495c3
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-02-12 13:40:34 -0500

    carousel for images

--- js/app.js
+++ js/app.js
@@ -23,17 +23,37 @@ function interestDetail(){
 
 $(document).ready(function(){
 	interestDetail();
-function flip(el){
-	el.children[1].style.webkitTransform = "perspective(600px) rotateY(-180deg)";
-	el.children[0].style.webkitTransform = "perspective(600px) rotateY(0deg)";
-	el.children[1].style.transform = "perspective(600px) rotateY(-180deg)";
-	el.children[0].style.transform = "perspective(600px) rotateY(0deg)";
-}
-function flipBack(el){
-	el.children[1].style.webkitTransform = "perspective(600px) rotateY(0deg)";
-	el.children[0].style.webkitTransform = "perspective(600px) rotateY(180deg)";
-	el.children[1].style.transform = "perspective(600px) rotateY(0deg)";
-	el.children[0].style.transform = "perspective(600px) rotateY(180deg)";
-}
+	function flip(el){
+		el.children[1].style.webkitTransform = "perspective(600px) rotateY(-180deg)";
+		el.children[0].style.webkitTransform = "perspective(600px) rotateY(0deg)";
+		el.children[1].style.transform = "perspective(600px) rotateY(-180deg)";
+		el.children[0].style.transform = "perspective(600px) rotateY(0deg)";
+	}
+	function flipBack(el){
+		el.children[1].style.webkitTransform = "perspective(600px) rotateY(0deg)";
+		el.children[0].style.webkitTransform = "perspective(600px) rotateY(180deg)";
+		el.children[1].style.transform = "perspective(600px) rotateY(0deg)";
+		el.children[0].style.transform = "perspective(600px) rotateY(180deg)";
+	}
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true
+	        },
+	        600:{
+	            items:3,
+	            nav:false
+	        },
+	        1000:{
+	            items:5,
+	            nav:true,
+	            loop:false
+        }
+    }
+})
 
 });
